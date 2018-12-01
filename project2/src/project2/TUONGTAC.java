b13edc95582793e6b588b535a1670c71f73dcad5
chinh12hy <42055241+chinh12hy@users.noreply.github.com>
2018-12-01 10:40:55 +0700

    đã coment

--- project2/src/project2/TUONGTAC.java
+++ project2/src/project2/TUONGTAC.java
@@ -26,7 +26,7 @@ public class TUONGTAC extends javax.swing.JFrame {
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
-
+		// da oment
         jLabel1 = new javax.swing.JLabel();
         jButton1 = new javax.swing.JButton();
         jButton2 = new javax.swing.JButton();
